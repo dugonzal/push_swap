16812e085a7a41ca597b2446a9a8eb3dd93760f7
dugonzal <ciclocontinuo2@gmail.com>
2022-11-17 19:44:26 +0100

    si solo es un argumeto, puede que nos hayan pasado una varibale con argumentos hace un split de los argumentos por cada espacio y los almacena cada uno como un puntero y retorna a argv para seguir con el control de errores

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 20:53:27 by vscode            #+#    #+#             */
-/*   Updated: 2022/11/17 19:17:26 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/11/17 19:26:44 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@
 # include "../libft/include/libft.h"
 
 //control de errores prototype
+
 void	error_msg(char *s, int n);
 void	err(const char **av, int i);
+void	repeat(t_list *a, int *tmp);
 
 #endif
